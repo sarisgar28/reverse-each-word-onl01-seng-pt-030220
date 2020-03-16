8fdd0ccd7c95a368c1c1eaf1d2a92c8e9227789e
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-16 16:27:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_world(phrase)
- reverse_each_world.each do |hello there,and how are you?| 
-    return reverse_each_world.reverse 
+ reverse_each_world.each do |greeting| 
+    return 
   end
 end  

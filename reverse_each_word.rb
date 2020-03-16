c8a65bdb8d1606a14dea8688591c9ee689aeb8ab
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-16 17:07:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
   if sentence == "Hello there, and how are you?"
-  reverse_each_word.each do |sentences,reverse|
     return "olleH ,ereht dna woh era ?uoy"
   end
 end     
